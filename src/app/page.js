4cd78e9613bbe1e6fb1166d6599e9b6f7703df18
SubRayZero <jasmine.draperi.simplon@gmail.com>
2024-05-23 16:09:29 +0200

    category ok

--- src/app/page.js
+++ src/app/page.js
@@ -1,59 +1,106 @@
+
 "use client"
-
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { Container, Card, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbars from '@/components/navbar';
 import HeroBanner from '@/components/heroBanner';
 import ReleaseDate from '@/components/releaseDate';
-import Navbars from '@/components/navbar';
 import TopRated from '@/components/topRate';
 import ListMovies from '@/components/movies';
 import SearchResult from '@/components/search';
 import PopularMovies from '@/components/popularMovies';
+import "./globals.css";
 
-export default function Home() {
+const API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YzI1MDI1ZTU5MjhhYjg5MGFmOWVkZTQyMjZiMjhkMSIsInN1YiI6IjY2NDFmZjQ3NDc0NmU5OGE4ZGZiNWJiZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UZkv8hTpx5fXs8Acln1k6I9w5RDTGjft5uZNED3OgvM';
+
+const fetchMoviesByGenre = async (genreId) => {
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${API_KEY}`,
+    },
+  };
+  const response = await fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=${genreId}`, options);
+  const data = await response.json();
+  return data.results;
+};
+
+const Home = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const [selectedGenre, setSelectedGenre] = useState(null);
+  const [movies, setMovies] = useState([]);
 
   const handleSearch = async (query) => {
     setSearchQuery(query);
-
     try {
-
       const response = await fetch(`https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1&query=${query}`, {
         method: 'GET',
         headers: {
           'Accept': 'application/json',
-          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YzI1MDI1ZTU5MjhhYjg5MGFmOWVkZTQyMjZiMjhkMSIsInN1YiI6IjY2NDFmZjQ3NDc0NmU5OGE4ZGZiNWJiZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UZkv8hTpx5fXs8Acln1k6I9w5RDTGjft5uZNED3OgvM'
+          'Authorization': `Bearer ${API_KEY}`
         }
       });
-
-
       const searchData = await response.json();
-
-
       setSearchResults(searchData.results);
     } catch (error) {
       console.error('Erreur lors de la recherche :', error);
     }
   };
 
-return  (
+  const handleSelectGenre = async (genreId) => {
+    setSelectedGenre(genreId);
+    const moviesByGenre = await fetchMoviesByGenre(genreId);
+    setMovies(moviesByGenre);
+  };
+
+  useEffect(() => {
+    if (selectedGenre !== null) {
+      handleSelectGenre(selectedGenre);
+    }
+  }, [selectedGenre]);
+
+  return (
     <>
-      <Navbars onSearch={handleSearch} />
+      <Navbars onSearch={handleSearch} onSelectGenre={handleSelectGenre} />
       {searchQuery ? (
         <SearchResult searchResults={searchResults} />
       ) : (
         <>
+          {selectedGenre && (
+            <Container>
+              <h1>Category : </h1>
+              <Row>
+                {movies.map((movie) => (
+                  <Col key={movie.id} md={4} lg={3} className="mb-4">
+                    <Card>
+                      <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+                      <Card.Body>
+                        <Card.Title>{movie.title}</Card.Title>
+                        <Card.Text>
+                          Release Date: {movie.release_date}
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                ))}
+              </Row>
+            </Container>
+          )}
           <HeroBanner />
           <div className="container_home">
             <ReleaseDate />
             <PopularMovies />
             <TopRated />
             <ListMovies />
+
           </div>
         </>
       )}
     </>
   );
+};
 
-}
+export default Home;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,29 @@
-// Navbars.js
-import React, { useState } from 'react';
-import { Button, Container, Form, Nav, Navbar } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Button, Container, Form, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 
-export default function Navbars({ onSearch }) {
+export default function Navbars({ onSearch, onSelectGenre }) {
     const [query, setQuery] = useState('');
+    const [genres, setGenres] = useState([]);
+
+    useEffect(() => {
+        const fetchGenres = async () => {
+            try {
+                const response = await fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', {
+                    method: 'GET',
+                    headers: {
+                        accept: 'application/json',
+                        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YzI1MDI1ZTU5MjhhYjg5MGFmOWVkZTQyMjZiMjhkMSIsInN1YiI6IjY2NDFmZjQ3NDc0NmU5OGE4ZGZiNWJiZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UZkv8hTpx5fXs8Acln1k6I9w5RDTGjft5uZNED3OgvM'
+                    }
+                });
+                const data = await response.json();
+                setGenres(data.genres);
+            } catch (error) {
+                console.error('Erreur lors de la récupération des genres :', error);
+            }
+        };
+
+        fetchGenres();
+    }, []);
 
     const handleSearchChange = (e) => {
         setQuery(e.target.value);
@@ -19,13 +39,19 @@ export default function Navbars({ onSearch }) {
             <Container fluid>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
-                    <Nav
-                        className="me-auto my-2 my-lg-0"
-                        style={{ maxHeight: '100px' }}
-                        navbarScroll
-                    >
-                        <Nav.Link href="../">Home</Nav.Link>
-                        <Nav.Link href="#action2">Category</Nav.Link>
+                    <Nav className="me-auto my-2 my-lg-0" navbarScroll>
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <NavDropdown title="Categories" id="navbarScrollingDropdown">
+                            {genres.length === 0 ? (
+                                <NavDropdown.Item disabled>Loading...</NavDropdown.Item>
+                            ) : (
+                                genres.map((genre) => (
+                                    <NavDropdown.Item key={genre.id} onClick={() => onSelectGenre(genre.id)}>
+                                        {genre.name}
+                                    </NavDropdown.Item>
+                                ))
+                            )}
+                        </NavDropdown>
                     </Nav>
                     <Form className="d-flex" onSubmit={handleSearchSubmit}>
                         <Form.Control
